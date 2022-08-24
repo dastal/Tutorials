10b5a438b363404b5917031658f5f8bd4e3fe6b8
David Stalder <43542299+dastal@users.noreply.github.com>
2022-08-24 16:19:07 +0200

    learned how to use variables in php

---
+++ php_Course/www/variables.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+    echo "There once was a man named George <br>";
+    echo "He was 70 years old <br>";
+    echo "He really liked the name George <br>";
+    echo "But didn't like being 70 years old <br>";
+
+    echo "<br>";
+
+    $characterName = "John";
+    $characterAge = 35;
+    echo "There once was a man named $characterName <br>";
+    echo "He was $characterAge years old <br>";
+    echo "He really liked the name $characterName <br>";
+    echo "But didn't like being $characterAge years old <br>";
+
+    echo "<br>";
+
+    $characterName = "Tom";
+    $characterAge = 80;
+    echo "There once was a man named $characterName <br>";
+    echo "He was $characterAge years old <br>";
+    $characterName = "Dave";
+    $characterAge = 31;
+    echo "He really liked the name $characterName <br>";
+    echo "But didn't like being $characterAge years old <br>";
+
+    ?>
+  </body>
+</html>
