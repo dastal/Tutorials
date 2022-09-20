2dda8acd8343c082e2382e48204998529a328ec3
David Stalder <43542299+dastal@users.noreply.github.com>
2022-09-20 21:06:43 +0200

    finished working with numbers

---
+++ PHP_Course/documents/workingWithNumbers.md
@@ -0,0 +1,49 @@
+# Working with Numbers #
+
+
+## Basic Maths ##
+There are two different types of numbers in PHP:
+
+- Integers
+- Decimal Numbers
+
+The mathematical operations are as usual:
+- Addition (+)
+- Subtraction (-)
+- Multiplication (*)
+- Division (/)
+- Modulo (%)
+
+The order of Operations is mathematical, i.e. for example multiplications before subtractions etc.
+
+Numbers can be stored in normal variables, like for example:
+
+```
+$num = 10;
+```
+
+The increment and the decrement of a number are as follows:
+
+```
+$num++;
+$num--;
+```
+
+The following operations are the same (the same counts for the subtraction):
+
+```
+$num = $num + 25;
+$num += 25;
+```
+
+## Advanced Maths ##
+For advanced maths there are math functions. Following are some of them, with a description if needed:
+
+- `abs(number)`
+- `pow(basis, exponent)`
+- `sqrt(number)`
+- `max(number1, number2)`
+- `min(number1, number2)`
+- `round(decimalNumber)`, rounds mathematically
+- `ceil(decimalNumber)`, rounds always up
+- `floow(decimalNumber)`, rounds always down
---
+++ PHP_Course/www/workingWithNumbers.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      echo 40;
+
+      echo "<br>";
+
+      echo -40;
+
+      echo "<br>";
+
+      echo -40.847;
+
+      echo "<br>";
+
+      echo 5 + 9;
+
+      echo "<br>";
+
+      echo 9 - 5;
+
+      echo "<br>";
+
+      echo 5 * 9;
+
+      echo "<br>";
+
+      echo 45 / 9;
+
+      echo "<br>";
+
+      echo 10 % 3;
+
+      echo "<br>";
+
+      echo 4 + 5 * 10;
+
+      echo "<br>";
+
+      echo (4 + 5) * 10;
+
+      echo "<br>";
+
+      $num = 10;
+
+      echo $num;
+
+      echo "<br>";
+
+      $num++;
+
+      echo $num;
+
+      echo "<br>";
+
+      $num--;
+
+      echo $num;
+
+      echo "<br>";
+
+      $num = $num + 25;
+
+      echo $num;
+
+      echo "<br>";
+
+      $num += 25;
+
+      echo $num;
+
+      echo "<br>";
+
+      $num -= 25;
+
+      echo $num;
+
+      echo "<br>";
+
+      echo abs(-100);
+
+      echo "<br>";
+
+      echo pow(2, 4);
+
+      echo "<br>";
+
+      echo sqrt(144);
+
+      echo "<br>";
+
+      echo max(2, 10);
+
+      echo "<br>";
+
+      echo min(2, 10);
+
+      echo "<br>";
+
+      echo round(3.2);
+
+      echo "<br>";
+
+      echo ceil(3.3);
+
+      echo "<br>";
+
+      echo floor(3.9);
+
+      echo "<br>";
+     ?>
+
+  </body>
+</html>
