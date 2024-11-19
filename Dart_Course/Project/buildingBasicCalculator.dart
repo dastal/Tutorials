969d35da13eb99115358c496d6bde4a69884d1f8
David Stalder <43542299+dastal@users.noreply.github.com>
2024-11-19 18:03:25 +0100

    Different ways of parsing numbers when receiving them as input (String)

--- Dart_Course/Project/buildingBasicCalculator.dart
+++ Dart_Course/Project/buildingBasicCalculator.dart
@@ -2,5 +2,33 @@ import 'dart:math';
 import 'dart:io';
 
 void main () {
-  
+  // Integers
+  print("Enter first number: ");
+  String? num1 = stdin.readLineSync();
+  print("Enter second number: ");
+  String? num2 = stdin.readLineSync();
+
+  print(
+      int.parse(num1!) + int.parse(num2!)
+  );
+
+  // Doubles
+  print("Enter first number: ");
+  String? num3 = stdin.readLineSync();
+  print("Enter second number: ");
+  String? num4 = stdin.readLineSync();
+
+  print(
+      double.parse(num3!) + double.parse(num4!)
+  );
+
+  // Parsing input
+  print("Enter first number: ");
+  double num5 = double.parse(stdin.readLineSync() ?? '0');
+  print("Enter second number: ");
+  double num6 = double.parse(stdin.readLineSync() ?? '0');
+
+  print(
+      num5 + num6
+  );
 }
